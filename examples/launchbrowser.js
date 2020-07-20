d5c34f25143cbaa3df6d49e17cfe97dff5f8f150
Rajeshkannan <rrajeshkannanbca@gmail.com>
2020-07-20 11:31:49 +0530

    fix: example import

--- examples/launchbrowser.js
+++ examples/launchbrowser.js
@@ -1,4 +1,4 @@
-const puppeteerUtil = require('./index')
+const puppeteerUtil = require('../index')
 async function test() {
   let browser =  await puppeteerUtil.launch({ headless: false, slowMo:       100, args: [ '--proxy-server="direct://"', '--proxy-bypass-list=*']})
   let page = await browser.newPage()
